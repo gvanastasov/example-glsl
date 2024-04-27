3c6e293180bf0e7597b548e67fe8ad6cf0d2bb4e
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-04-27 22:04:26 +0200

    update pendulum matrix

--- src/07-1_matrix_pendulum.glsl
+++ src/07-1_matrix_pendulum.glsl
@@ -16,12 +16,14 @@ float maskCircle(in vec2 space, in vec2 pos, in float radius, in float feather) 
     return 1.0 - smoothstep(radius-feather, radius, dot(dist,dist) * 4.0);
 }
 
+mat2 pendelumMatrix() {
+    float angle = sin(u_time * FREQUENCE) * PI * ARC_ANGLE / 360.;
+    return mat2(sin(angle), 0, 0, cos(angle)) * 0.5 * ARM_LEN;
+}
+
 void main() {
     vec2 st = gl_FragCoord.xy / u_resolution;
-    float angle = sin(u_time * FREQUENCE) * PI * ARC_ANGLE / 360.;
-
-    vec2 translate = vec2(sin(angle), cos(angle)) * 0.5 * ARM_LEN;
-    st += translate;
+    st += pendelumMatrix() * vec2(1.0, 1.0);
 
     /*
         Notice we dont move the center of the circle, we move the origin of the space
