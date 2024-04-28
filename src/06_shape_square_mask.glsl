c400c51fefc97f7d4dab8fc762df4d9c7e6c3196
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-04-28 20:55:07 +0200

    add improved feathered square mask

--- src/06_shape_square_mask.glsl
+++ src/06_shape_square_mask.glsl
@@ -33,12 +33,21 @@ float squareMaskSmooth(in vec2 st, in float size) {
     return bl.x * bl.y * tr.x * tr.y;
 }
 
+/*
+    Calculate a smooth (feathered) square mask with given position
+    and size within parent 2D system.
+*/
+float maskRectSmooth(in vec2 space, in vec2 center, in vec2 size, float feather) {
+    vec2 dist = abs(space - center) - size * 0.5;
+    return smoothstep(feather, 0.0, max(dist.x, dist.y));
+}
+
 void main() {
     vec2 st = gl_FragCoord.xy / u_resolution.xy;
 
     float inset = squareMask(st, 0.1);
     float insetSmooth = squareMaskSmooth(st, 0.1);
-    float rectMask = squareMask(st, vec2(0.3, 0.3), 0.3);
+    float rectMask = maskRectSmooth(st, vec2(0.3, 0.2), vec2(0.3, 0.1), 0.01);
 
     vec3 color = mix(vec3(1.0, 0.0, 0.0), vec3(0.0), insetSmooth);
     color = mix(color, vec3(0.0, 0.0, 1.0), inset);
