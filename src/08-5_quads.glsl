ee8f0c349ecedb1aaa3786ebedb0cf1a0e5157da
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-04-28 21:22:49 +0200

    add animation to quads

--- src/08-5_quads.glsl
+++ src/08-5_quads.glsl
@@ -5,6 +5,7 @@
 #define PI 3.14159265358979323846
 
 uniform vec2 u_resolution;
+uniform float u_time;
 
 vec2 rotate2D(vec2 _st, float _angle){
     _st -= 0.5;
@@ -24,16 +25,25 @@ float maskRectSmooth(in vec2 space, in vec2 center, in vec2 size, float feather)
     return smoothstep(feather, 0.0, max(dist.x, dist.y));
 }
 
+vec2 scale(vec2 _st, vec2 _scale) {
+    _st -= 0.5;
+    _st = mat2(_scale.x, 0.0,
+                0.0, _scale.y) * _st;
+    _st += 0.5;
+    return _st;
+}
+
 void main() {
 	vec2 st = gl_FragCoord.xy / u_resolution;
 
     st = tile(st, 4.);
-    st = rotate2D(st,PI*0.25);
+    st = rotate2D(st, 2.0 * PI * 0.25 * fract(u_time * 0.5));
+    st = scale(st, vec2(2.0) + sin(u_time));
 
     float quad = maskRectSmooth(st, vec2(0.5), vec2(0.7), 0.01);
 
     vec3 color = vec3(0.0);
-    color = vec3(quad);
+    color = vec3(quad * (1. - sin(u_time)));
 
 	gl_FragColor = vec4(color, 1.0);
 }
